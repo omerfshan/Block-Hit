84ea140846d6bfc9b46857d29c7f75d27ca36e02
sahan <101819553+omerfshan@users.noreply.github.com>
2025-01-20 09:08:20 +0300

    change project

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+
+    private int count;
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        if(transform.position.y<=-10)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void SetStartingCount(int count)
+    {
+      this.count=count;
+    }
+   private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.collider.name=="Ball"&&count>0){
+            count--;
+            if(count==0){
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] bloks;
+    public List<GameObject> levels;
+    private GameObject level1;
+    private GameObject level2;
+    private Vector2 level1Pos;
+    private Vector2 level2Pos;
+
+    void Start()
+    {
+        Physics2D.gravity=new Vector2(0,-17);
+        SpawnNewLevel(0,17,3,5);
+    }
+
+ 
+    void Update()
+    {
+        
+    }
+    void SpawnNewLevel(int numberLevel1,int numberLevel2,int min,int max){
+        level1Pos=new Vector2(3.5f,1);
+        level2Pos=new Vector2(3.5f,-3.4f);
+
+        level1=levels[numberLevel1];
+        level2=levels[numberLevel2];
+
+        Instantiate(level1,level1Pos,Quaternion.identity);
+        Instantiate(level2,level2Pos,Quaternion.identity);
+        SetBlocksCount(min,max);
+    }
+    void SetBlocksCount(int min,int max){
+        bloks=GameObject.FindGameObjectsWithTag("Block");
+        for(int i=0; i<bloks.Length; i++){
+            int count=Random.Range(min,max);
+            bloks[i].GetComponent<Block>().SetStartingCount(count);
+        }
+    }
+}
